bf3bb76f32b334a980e3220ef4fa54f67e456894
zero_0_1103 <83809838+dlwlsdudo1@users.noreply.github.com>
2021-07-04 00:45:51 +0900

    Node Node Node
    gggggggg

--- 0703.js
+++ 0703.js
@@ -1,21 +1,48 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
+/*app.get('/notice', (req, res) => {
   res.send('Hello World!')
   console.log('hello');
 })
 
-app.post('/dd', (req, res) => {
+app.post('/notices', (req, res) => {
     res.send('~!')
     console.log('hello');
   })
 
-var url = require('url');
-
-var parsedURL = url.parse('http://www.google.co.kr')
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+})*/
+
+
+const express = require('express')
+const app = express()
+const port = 3308 // db port 
+
+var mysql = require('mysql');
+
+/*var app = express();
+http.createServer(app).listen(3306, function(){
+  console.log('Server running at http://127.0.0.1:3306');
+});*/
+
+var con = mysql.createConnection({
+  host: "db",
+  user: "name",
+  password: "password",
+  database : 'database'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+con.query('select * from ...', (error, results, fields)=> {
+  if (error) {
+      console.log(error);
+  }
+  console.log(results);
+});
+
+con.end();
+
+//('insert into ... (a, b, c) VALUES ("Test", "Test1", "Test2")'
